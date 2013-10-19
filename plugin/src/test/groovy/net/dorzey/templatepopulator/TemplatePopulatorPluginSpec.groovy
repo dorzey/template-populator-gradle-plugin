49993cddb565d7ee065986db2570d4dfa43becea
Paul Doran <dorzey@gmail.com>
2013-10-19 12:58:14 +0100

    Switching to Spock.

---
+++ plugin/src/test/groovy/net/dorzey/templatepopulator/TemplatePopulatorPluginSpec.groovy
@@ -0,0 +1,17 @@
+package net.dorzey.templatepopulator
+
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class TemplatePopulatorPluginSpec extends Specification {
+    def project = ProjectBuilder.builder().build()
+
+    def setup() {
+        project.apply plugin: 'template-populator'
+    }
+
+    def "should have populateTemplates task"() {
+        expect:
+        project.tasks.populateTemplates instanceof PopulateTemplatesTask
+    }
+}
